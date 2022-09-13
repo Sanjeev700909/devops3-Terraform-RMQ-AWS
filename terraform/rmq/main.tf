180d26bead3f7f5236c734454a0992be36740fa3
Sanjeev700909 <106360480+Sanjeev700909@users.noreply.github.com>
2022-09-13 17:46:27 -0400

    Update main.tf

--- terraform/rmq/main.tf
+++ terraform/rmq/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-    region="us-east-1"
+    region="us-west-2"
     profile=var.profile
 }
 
 resource "aws_instance" "rmq" {
     ami = "ami-07ebfd5b3428b6f4d"
     instance_type = "t2.micro"
-    key_name = "rabbitmq"
-    vpc_security_group_ids = [ "sg-061a88413e99c4cf1" ]
+    key_name = "NewKey"
+    vpc_security_group_ids = [ "sg-0165b85f9d9cdb796" ]
 
     tags = {
         Name = var.name
